fd56b364391c7815038abb2fcfca59da369446c2
ItsSalah <s96zakaria@gmail.com>
2020-01-10 13:52:25 +0100

    image Article

--- src/main/java/com/wedding/demo/controllers/ArticleController.java
+++ src/main/java/com/wedding/demo/controllers/ArticleController.java
@@ -2,22 +2,34 @@ package com.wedding.demo.controllers;
 
 import com.wedding.demo.exceptions.ResourceNotFoundException;
 import com.wedding.demo.models.Article;
+import com.wedding.demo.models.Image;
+import com.wedding.demo.models.Offre;
 import com.wedding.demo.repositories.ArticleRepository;
+import com.wedding.demo.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.Valid;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 
 @RestController
-@RequestMapping("/contact")
+@RequestMapping("/article")
 @CrossOrigin("*")
 public class ArticleController {
 
     @Autowired
     private ArticleRepository articleRepository;
+
+
+    @Autowired
+    private ImageRepository imageRepository;
 
     @GetMapping("/")
     public ResponseEntity<List<Article>>  getContacts() {
@@ -62,5 +74,23 @@ public class ArticleController {
 
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
+    @GetMapping(path="/articleImage/{id}",produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getPhoto(@PathVariable("id") Long id) throws Exception{
+        Image p=imageRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Contact "+ id+" not found" ));;
+        return Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/images/article/"+p.getLink()));
+    }
 
+    @PostMapping("/articleImage/{id}")
+    public Image uploadFile(@RequestParam("file") MultipartFile file, @PathVariable("id") Long id)  throws Exception{
+        Article article =articleRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Article "+ id+" not found" ));
+
+        Files.write(Paths.get(System.getProperty("user.dir")+"/images/article/"+file.getOriginalFilename()),file.getBytes());
+        Image image = new Image();
+        image.setLink(file.getOriginalFilename());
+
+        return imageRepository.save(image);
+    }
 }
